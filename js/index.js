e08a8ab114012b92ed8e6d0744bc0d8987432061
Angelo <ben7152000@gmail.com>
2023-09-02 12:37:06 +0800

    fixed: 邏輯調整

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
-// 常數
+/**
+ * 常數
+ */
 const SPACE = 'Space'
 const FLEX = 'flex'
 const NONE = 'none'
 
-// Dom
+/**
+ * Dom
+ */
 const mainInfo = document.querySelector('.main-info')
 const gameInfo = document.querySelector('.game-info')
 const resultInfo = document.querySelector('.result-info')
@@ -23,61 +27,133 @@ const checkFail = document.querySelector('#check-fail')
 const time = document.querySelector('.time')
 const timerBar = document.querySelector('.timer-bar')
 
-// 變數
+/**
+ * 變數
+ */
 const symbols = ['▲', '●', '★', '✚', '♦']
 let correctCounts = 0
 let incorrectCounts = 0
 let missedCounts = 0
 let performanceRate = 0
+let getRandomCounts = 0
 let leftSymbolItem = ''
 let rightSymbolItem = ''
-let alreadyEqual = false
-const gameTime = 2 // 分鐘
+let alreadyEqual = false // 是否點擊相同
+const gameTime = 0.3 // 分鐘
 const randomSymbolTime = 1 // 秒
 const delayEqualTime = 2 // 秒
+let randomInterval // 隨機倒數定時器
 
-// 監聽
+/**
+ * 監聽
+ */
 window.addEventListener('keydown', handleKeydown)
 start.addEventListener('click', startClickHandler)
 equal.addEventListener('click', equalClickHandler)
 restart.addEventListener('click', restartClickHandler)
 
-// 開始遊戲
-function startClickHandler () {
-  mainInfo.style.display = NONE
-  gameInfo.style.display = FLEX
-  startCountdown(gameTime)
-}
-
-// 重新開始
-function restartClickHandler() {
+/**
+ * 進到介紹介面
+ */
+function goToInstructionPage() {
   resultInfo.style.display = NONE
   mainInfo.style.display = FLEX
-  resetResult()
 }
 
-// 點擊空白鍵
+/**
+ * 進到遊戲介面
+ */
+function goToGamePage() {
+  mainInfo.style.display = NONE
+  gameInfo.style.display = FLEX
+}
+
+/**
+ * 進到結果介面
+ */
+function goToResultPage() {
+  gameInfo.style.display = NONE
+  resultInfo.style.display = FLEX
+}
+
+/**
+ * 點擊空白鍵
+ */
 function handleKeydown(e) {
   if (e.code === SPACE) {
     equalClickHandler()
   }
 }
 
-// 隨機取值
+/**
+    重新開始
+ */
+function restartClickHandler() {
+  goToInstructionPage()
+  resetResult()
+}
+
+/**
+    重置結果數據
+ */
+function resetResult() {
+  correctCounts = 0
+  incorrectCounts = 0
+  missedCounts = 0
+  performanceRate = 0
+  getRandomCounts = 0
+  timerBar.style.width = '0'
+}
+
+/**
+ * 開始遊戲
+ */
+function startClickHandler () {
+  goToGamePage()
+  getSymbols()
+  startCountdown(gameTime)
+  time.innerHTML = (Math.floor(gameTime) + 1).toString()
+  randomGetSymbols()
+}
+
 function randomGetSymbols() {
+  clearInterval(randomInterval)
+  randomInterval = setInterval(() => {
+    if (alreadyEqual) return
+    getSymbols()
+  }, randomSymbolTime * 1000)
+}
+
+/**
+ * 隨機取值
+ */
+function randomSymbol() {
   const randomIndex = Math.floor(Math.random() * symbols.length)
   return symbols[randomIndex]
 }
 
-// 取亂數
-function randomGetSymbol() {
-  leftSymbolItem = randomGetSymbols()
-  rightSymbolItem = randomGetSymbols()
+/**
+ * 取亂數顯示
+ */
+function getSymbols() {
+  getRandomCounts++
+  leftSymbolItem = randomSymbol()
+  rightSymbolItem = randomSymbol()
   leftSymbol.innerHTML = leftSymbolItem
   rightSymbol.innerHTML = rightSymbolItem
 }
 
-// 是否相同
+/**
+ * 選中相同
+ */
+function addActive(targetClass) {
+  leftCard.classList.add(targetClass)
+  rightCard.classList.add(targetClass)
+}
+
+/**
+ * 點擊是否相同
+ */
 function equalClickHandler() {
   if (alreadyEqual) return
   alreadyEqual = true
@@ -92,19 +168,18 @@ function equalClickHandler() {
     addActive('bg-red')
   }
 
+  clearInterval(randomInterval)
+
   setTimeout(() => {
     removeActive()
-    // randomGetSymbol()
+    getSymbols()
+    randomGetSymbols()
   }, delayEqualTime * 1000)
 }
 
-// 選中相同
-function addActive(targetClass) {
-  leftCard.classList.add(targetClass)
-  rightCard.classList.add(targetClass)
-}
-
-// 移除相同
+/**
+ * 移除相同
+ */
 function removeActive() {
   alreadyEqual = false
   checkTrue.style.display = NONE
@@ -115,8 +190,11 @@ function removeActive() {
   rightCard.classList.remove('bg-red')
 }
 
-// 輸出結果
+/**
+ * 輸出結果
+ */
 function outputResult() {
+  missedCounts = getRandomCounts - correctCounts - incorrectCounts
   performanceRate = Math.round((correctCounts / (correctCounts + incorrectCounts + missedCounts)) * 100) || 0
   correct.innerHTML = correctCounts.toString()
   incorrect.innerHTML = incorrectCounts.toString()
@@ -124,16 +202,9 @@ function outputResult() {
   performance.innerHTML = `${performanceRate.toString()}%`
 }
 
-// 重置結果數據
-function resetResult() {
-  correctCounts = 0
-  incorrectCounts = 0
-  missedCounts = 0
-  performanceRate = 0
-  timerBar.style.width = '0'
-}
-
-// 倒數計時器
+/**
+ *  倒數計時
+ */
 function startCountdown(duration) {
   let timer = duration * 60
   let timeWidth = 100
@@ -153,19 +224,15 @@ function startCountdown(duration) {
     }
 
     if (timer === 0) {
-      gameInfo.style.display = NONE
-      resultInfo.style.display = FLEX
+      goToResultPage()
       outputResult()
       clearInterval(interval)
     } else {
       timer--
-      if (alreadyEqual) return
-      missedCounts++
-      randomGetSymbol()
     }
   }
 
   updateCountdown()
-  const interval = setInterval(updateCountdown, randomSymbolTime * 1000)
+  const interval = setInterval(updateCountdown, 1000)
 }
 
